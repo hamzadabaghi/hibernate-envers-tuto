ba14d5d1876b8879a9137b7355ff30f161fa453d
= <=>
2023-03-01 11:31:59 +0100

    [HT-3] : add getters and setters to the abstract entity class.

--- src/main/java/com/hibernateenverstuto/model/AbstractEntity.java
+++ src/main/java/com/hibernateenverstuto/model/AbstractEntity.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+@Entity @Getter @Setter
 public class AbstractEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
