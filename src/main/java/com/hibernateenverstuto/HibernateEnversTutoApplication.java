b7f0d738cbae3275727d3fa947dea2b98073b5c1
Hamza DABAGHI <hdabaghi@leyton.com>
2023-03-01 10:00:11 +0100

    [HT-1] : initializing the project.

---
+++ src/main/java/com/hibernateenverstuto/HibernateEnversTutoApplication.java
@@ -0,0 +1,13 @@
+package com.hibernateenverstuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnversTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateEnversTutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hibernateenverstuto/HibernateEnversTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernateenverstuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEnversTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
