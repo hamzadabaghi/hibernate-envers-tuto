07d88c6cca6ea8df1682688cc8ed99cdfa05c0bc
= <=>
2023-03-01 11:27:05 +0100

    [HT-2] : add abstract entity class.

---
+++ src/main/java/com/hibernateenverstuto/model/AbstractEntity.java
@@ -0,0 +1,12 @@
+package com.hibernateenverstuto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AbstractEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
